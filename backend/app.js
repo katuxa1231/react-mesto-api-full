e2651af321477c7dc9cffab1c6679320ddeb2c73
Ekaterina Kuznetsova <katuxa1231@yandex.ru>
2022-09-17 22:46:51 +0400

    Добавляет cors middleware

--- backend/app.js
+++ backend/app.js
@@ -4,32 +4,19 @@ const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const { celebrate, errors } = require('celebrate');
 const cookieParser = require('cookie-parser');
-const cors = require('cors');
 const { StatusCode } = require('./constants/api');
 const { login, createUser } = require('./controllers/users');
 const { auth } = require('./midlewares/auth');
 const { handleError } = require('./midlewares/error');
 const { requestLogger, errorLogger } = require('./midlewares/logger');
 const { validationModel } = require('./constants/validation');
+const { cors } = require('./midlewares/cors');
 require('dotenv').config();
 
 const { PORT = 3000 } = process.env;
 
-const options = {
-  origin: [
-    'http://localhost:3200',
-    'https://mesto1231.nomoredomains.sbs',
-    'https://katuxa1231.github.io',
-  ],
-  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
-  credentials: true,
-};
-
 const app = express();
-app.use(cors(options));
+app.use(cors);
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
